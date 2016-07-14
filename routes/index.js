33989665b9719cf97364c03a3f61d5b8f2c371a7
quachduyy <qdragonjr@gmail.com>
2016-07-15 01:59:29 +0700

    n

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+<<<<<<< HEAD
 router.get('/about', function (req, res) {
     res.send('Hello!!!');
 });
 
+=======
+>>>>>>> parent of 3aea2f8... fisrt
 module.exports = router;
